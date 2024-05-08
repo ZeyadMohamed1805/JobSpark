6a11eebab29fbff3953d1b4196362b0060e90142
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-09 02:15:00 +0300

    Completed The Account Page's Skills Component

---
+++ src/app/components/account/skills/skills.component.constants.ts
@@ -0,0 +1,11 @@
+export const skillsData: Array<string> = [
+	'HTML',
+	'CSS',
+	'JavaScript',
+	'TypeScript',
+	'C#',
+	'Node JS',
+	'Entity Framework',
+	'SQL Server',
+	'Mongo DB',
+];
--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { IntroComponent } from '../../components/account/intro/intro.component';
 import { ProfileComponent } from '../../components/account/profile/profile.component';
 import { ContactComponent } from '../../components/account/contact/contact.component';
+import { SkillsComponent } from '../../components/account/skills/skills.component';
 
 @Component({
 	selector: 'app-account',
 	standalone: true,
-	imports: [IntroComponent, ProfileComponent, ContactComponent],
+	imports: [
+		IntroComponent,
+		ProfileComponent,
+		ContactComponent,
+		SkillsComponent,
+	],
 	templateUrl: './account.component.html',
 	styleUrl: './account.component.scss',
 })
