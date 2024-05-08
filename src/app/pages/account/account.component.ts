2ce5035e6e75f4baabfcbdfa6757c0b377f5eabb
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-09 00:34:04 +0300

    Initialized Account Page's Profile Component

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IntroComponent } from '../../components/account/intro/intro.component';
+import { ProfileComponent } from '../../components/account/profile/profile.component';
 
 @Component({
 	selector: 'app-account',
 	standalone: true,
-	imports: [IntroComponent],
+	imports: [IntroComponent, ProfileComponent],
 	templateUrl: './account.component.html',
 	styleUrl: './account.component.scss',
 })
