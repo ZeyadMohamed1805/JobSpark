e0b11998c0b032dd13b6c225d9572741ccd1e745
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-09 01:53:36 +0300

    Completed The Account Page's Contact Component

---
+++ src/app/components/account/contact/contact.component.constants.ts
@@ -0,0 +1,8 @@
+import { TContactData } from './contact.component.types';
+
+export const contactData: Array<TContactData> = [
+	{ type: 'Website', value: 'http://www.zeyad-mohamed.com' },
+	{ type: 'Email', value: 'zeyad-mohamed@email.com' },
+	{ type: 'Github', value: 'https://www.zeyad-mohamed.github.io' },
+	{ type: 'Phone', value: '0123456789' },
+];
--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IntroComponent } from '../../components/account/intro/intro.component';
 import { ProfileComponent } from '../../components/account/profile/profile.component';
+import { ContactComponent } from '../../components/account/contact/contact.component';
 
 @Component({
 	selector: 'app-account',
 	standalone: true,
-	imports: [IntroComponent, ProfileComponent],
+	imports: [IntroComponent, ProfileComponent, ContactComponent],
 	templateUrl: './account.component.html',
 	styleUrl: './account.component.scss',
 })
