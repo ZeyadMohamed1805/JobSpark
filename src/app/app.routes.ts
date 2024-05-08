ebdc4c86b0923f1449a58d67606508b8a32aa75b
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-08 18:25:49 +0300

    Initialized The Account Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { JobsComponent } from './pages/jobs/jobs.component';
+import { AccountComponent } from './pages/account/account.component';
 
 export const routes: Routes = [
 	{ path: '', pathMatch: 'full', redirectTo: 'home' },
 	{ path: 'home', component: HomeComponent },
 	{ path: 'jobs', component: JobsComponent },
+	{ path: 'account', component: AccountComponent },
 ];
