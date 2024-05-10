a1e55f61060a8203dc18892e0efa0f3fb06ccd33
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-10 18:18:35 +0300

    Initalized The Filter Component

--- src/app/components/jobs/options/options.component.ts
+++ src/app/components/jobs/options/options.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatTabGroup, MatTab } from '@angular/material/tabs';
 import { SortComponent } from '../sort/sort.component';
+import { FilterComponent } from '../filter/filter.component';
 
 @Component({
 	selector: 'app-options',
 	standalone: true,
-	imports: [MatTabGroup, MatTab, SortComponent],
+	imports: [MatTabGroup, MatTab, SortComponent, FilterComponent],
 	templateUrl: './options.component.html',
 	styleUrl: './options.component.scss',
 })
