6af7496480818df0b39409359d45187bc7b65b1b
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-10 18:15:03 +0300

    Initialized The Sort Component

--- src/app/components/jobs/options/options.component.ts
+++ src/app/components/jobs/options/options.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatTabGroup, MatTab } from '@angular/material/tabs';
+import { SortComponent } from '../sort/sort.component';
 
 @Component({
 	selector: 'app-options',
 	standalone: true,
-	imports: [MatTabGroup, MatTab],
+	imports: [MatTabGroup, MatTab, SortComponent],
 	templateUrl: './options.component.html',
 	styleUrl: './options.component.scss',
 })
