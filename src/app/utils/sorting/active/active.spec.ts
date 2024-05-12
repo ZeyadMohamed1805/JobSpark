6864062112cc9aa0db6d9dcb34577cfc4fa1712f
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-12 12:48:03 +0300

    Updated Active Logic To Reviewed Percentage Of Total Applicants

--- src/app/utils/sorting/active/active.spec.ts
+++ src/app/utils/sorting/active/active.spec.ts
@@ -2,16 +2,32 @@ import { active } from './active';
 import { EOrderType } from '../sorting.types';
 
 describe('active', () => {
-	it('should return the data sorted from least reviewed to most reviewed', () => {
-		const data = [{ reviews: 2 }, { reviews: 3 }, { reviews: 1 }];
-		const expected = [{ reviews: 1 }, { reviews: 2 }, { reviews: 3 }];
+	it('should return the data sorted from least active to most active', () => {
+		const data = [
+			{ reviews: 2, applicants: 5 },
+			{ reviews: 3, applicants: 5 },
+			{ reviews: 1, applicants: 5 },
+		];
+		const expected = [
+			{ reviews: 1, applicants: 5 },
+			{ reviews: 2, applicants: 5 },
+			{ reviews: 3, applicants: 5 },
+		];
 		const result = active(data, EOrderType.ASC);
 		expect(result).toEqual(expected);
 	});
 
-	it('should return the data sorted from most reviewed to least reviewed', () => {
-		const data = [{ reviews: 2 }, { reviews: 3 }, { reviews: 1 }];
-		const expected = [{ reviews: 3 }, { reviews: 2 }, { reviews: 1 }];
+	it('should return the data sorted from most active to least active', () => {
+		const data = [
+			{ reviews: 2, applicants: 5 },
+			{ reviews: 3, applicants: 5 },
+			{ reviews: 1, applicants: 5 },
+		];
+		const expected = [
+			{ reviews: 3, applicants: 5 },
+			{ reviews: 2, applicants: 5 },
+			{ reviews: 1, applicants: 5 },
+		];
 		const result = active(data, EOrderType.DESC);
 		expect(result).toEqual(expected);
 	});
--- src/app/utils/sorting/active/active.ts
+++ src/app/utils/sorting/active/active.ts
@@ -3,9 +3,15 @@ import { EOrderType } from '../sorting.types';
 export const active = (data: Array<any>, order: EOrderType) => {
 	return order === EOrderType.ASC
 		? data.sort((elementOne, elementTwo) =>
-				elementOne.reviews > elementTwo.reviews ? 1 : -1
+				elementOne.reviews / elementOne.applicants >
+				elementTwo.reviews / elementTwo.applicants
+					? 1
+					: -1
 		  )
 		: data.sort((elementOne, elementTwo) =>
-				elementOne.reviews < elementTwo.reviews ? 1 : -1
+				elementOne.reviews / elementOne.applicants <
+				elementTwo.reviews / elementTwo.applicants
+					? 1
+					: -1
 		  );
 };
