38f828b8ba0def71194dc29a159e2f5a82f2c041
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-12 13:05:14 +0300

    Added All Sorting Utilities To The Order Service

--- src/app/services/order/order.service.ts
+++ src/app/services/order/order.service.ts
@@ -3,20 +3,25 @@ import { EOrderType } from '../../utils/sorting/sorting.types';
 import { alphabet } from '../../utils/sorting/alphabet/alphabet';
 import { recent } from '../../utils/sorting/recent/recent';
 import { active } from '../../utils/sorting/active/active';
+import { companySize } from '../../utils/sorting/company-size/company-size';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class OrderService {
-	alphabetically(data: Array<any>, order: EOrderType) {
+	byAlphabet(data: Array<any>, order: EOrderType) {
 		return alphabet(data, order);
 	}
 
-	recent(data: Array<any>, order: EOrderType) {
+	byRecent(data: Array<any>, order: EOrderType) {
 		return recent(data, order);
 	}
 
-	active<T>(data: Array<T>, order: EOrderType): Array<T> {
+	byCompanySize<T>(data: Array<T>, order: EOrderType): Array<T> {
+		return companySize(data, order);
+	}
+
+	byActive<T>(data: Array<T>, order: EOrderType): Array<T> {
 		return active(data, order);
 	}
 }
