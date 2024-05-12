c2df1c5942125357aec5c895619ac5805b8ed1c6
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-12 11:40:00 +0300

    Declared The Order Service's Methods

--- src/app/services/order/order.service.ts
+++ src/app/services/order/order.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class OrderService {
+	alphabet<T>(data: Array<T>, order: string): Array<T> {
+		return data;
+	}
 
-  constructor() { }
+	recent<T>(data: Array<T>, order: string): Array<T> {
+		return data;
+	}
+
+	rate<T>(data: Array<T>, order: string): Array<T> {
+		return data;
+	}
+
+	salary<T>(data: Array<T>, order: string): Array<T> {
+		return data;
+	}
+
+	active<T>(data: Array<T>, order: string): Array<T> {
+		return data;
+	}
 }
