c8c053ba65b52efc4ee57b1e8a9c06e4cc1efb73
Tarek <tareknasr1994@gmail.com>
2024-05-18 19:16:08 +0300

    fetched vacancy card from api

--- src/app/components/home/vacancies/vacancies.component.ts
+++ src/app/components/home/vacancies/vacancies.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { HomeVacancyCardComponent } from '../../common/cards/home-vacancy-card/home-vacancy-card.component';
 import { TVacancy } from './vacancies.component.types';
@@ -6,31 +7,46 @@ import { cardsData } from './vacancies.component.constants';
 import { THomeVacancyDTO } from '../../../types/dtos/home-vacany';
 import { OrderService } from '../../../services/order/order.service';
 import { EOrderType } from '../../../utils/sorting/sorting.types';
+import { ApiService } from '../../../services/api/api.service';
 
 @Component({
-	selector: 'app-vacancies',
-	standalone: true,
-	imports: [TitleCasePipe, HomeVacancyCardComponent],
-	templateUrl: './vacancies.component.html',
-	styleUrl: './vacancies.component.scss',
+  selector: 'app-vacancies',
+  standalone: true,
+  imports: [CommonModule, TitleCasePipe, HomeVacancyCardComponent],
+  templateUrl: './vacancies.component.html',
+  styleUrls: ['./vacancies.component.scss'],
 })
 export class VacanciesComponent implements OnInit {
-	@Input() data: TVacancy | null = null;
-	cardsData: Array<THomeVacancyDTO> = [];
+  @Input() data: TVacancy | null = null;
+  cardsData: Array<THomeVacancyDTO> = [];
 
-	constructor(private orderService: OrderService) {}
+  constructor(private orderService: OrderService, private apiService: ApiService) {}
 
-	ngOnInit(): void {
-		if (this.data?.sortingType === 'most-recent') {
-			this.cardsData = this.orderService.byRecent(
-				cardsData,
-				EOrderType.ASC
-			);
-		} else if (this.data?.sortingType === 'alphabetically') {
-			this.cardsData = this.orderService.byAlphabet(
-				cardsData,
-				EOrderType.ASC
-			);
-		}
-	}
+  ngOnInit(): void {
+
+
+    // Fetch data from API
+    this.fetchVacancies();
+  }
+
+  fetchVacancies(): void {
+    this.apiService.get<any>('api/Vacancy').subscribe(
+      (data) => {
+		console.log(data.data);
+      //  this.cardsData = data;
+	  if (this.data?.sortingType === 'most-recent') {
+		this.cardsData = this.orderService.byRecent(data.data, EOrderType.ASC);
+	  } else if (this.data?.sortingType === 'alphabetically') {
+		this.cardsData = this.orderService.byAlphabet(data.data, EOrderType.ASC);
+	  }
+
+
+      },
+      (error) => {
+        console.error('Error fetching vacancies:', error);
+      }
+    );
+  }
+
+
 }
